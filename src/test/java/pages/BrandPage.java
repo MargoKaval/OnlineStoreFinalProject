70a06d6f724cca00d1b129a4ffff7dde90a28671
margo <margo8ivanova@gmail.com>
2023-06-27 21:52:25 +0300

    B2: Final project (removed manual sleep, added centralized sleep from Utils.Wait, added assertion)

--- src/test/java/pages/BrandPage.java
+++ src/test/java/pages/BrandPage.java
@@ -3,18 +3,18 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import static java.lang.Thread.sleep;
 import static utils.DriverProvider.getCurrentDriver;
+import static utils.Wait.waitForPageToLoad;
 
 public class BrandPage {
     private String checkboxWheyProteinXpath = "//li/label/input[@value='217']";
     private String dropdownSortByXpath = "//div[@class='btn-group f-lang-switch-form__select']/button[@class='btn btn-default btn-sm dropdown-toggle']";
     private String highPriceSortingCSS = "a[ng-click=\"$event.preventDefault(); grid.onSelectSorting('price', 'desc');\"]";
 
-    public void clickCheckbox() throws InterruptedException {
+    public void clickCheckbox() {
         WebElement checkboxWheyProtein = getCurrentDriver().findElement(By.xpath(checkboxWheyProteinXpath));
         checkboxWheyProtein.click();
-        sleep(3000);
+        waitForPageToLoad();
     }
 
     public void sortByHighPrice() {
@@ -23,5 +23,7 @@ public class BrandPage {
 
         WebElement highPriceSorting = getCurrentDriver().findElement(By.cssSelector(highPriceSortingCSS));
         highPriceSorting.click();
+        //need this sleep to be able to click on specific product otherwise test fails
+        waitForPageToLoad();
     }
 }
