4fd47bdd6ca71ad3fd54d0d8c7efaccf3f1be5db
margo <margo8ivanova@gmail.com>
2023-06-27 21:54:34 +0300

    B2: Final project (created page)

--- src/test/java/pages/ProductPage.java
+++ src/test/java/pages/ProductPage.java
@@ -2,13 +2,21 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
+import static utils.Wait.waitForPageToLoad;
 import static utils.DriverProvider.getCurrentDriver;
 
 public class ProductPage {
-    private String specificProductXpath = "//div/a[@href='/en/products/gaspari-nutrition-proven-whey']";
+    private String specificProductXpath = "//a[@href='/en/products/scitec-nutrition-100%25-whey-isolate-2000-g']";
+    private String addToCartBtnXpath = "//form[@name='AddToCartForm']/button[@type='submit']";
 
-    /*public void clickOnSpecificProduct() {
-        WebElement specificProduct = getCurrentDriver().findElement(By.cssSelector(specificProductXpath));
+    public void clickOnSpecificProduct() {
+        WebElement specificProduct = getCurrentDriver().findElement(By.xpath(specificProductXpath));
         specificProduct.click();
-    }*/
+    }
+    public void addToCart() {
+        WebElement addToCartBtn = getCurrentDriver().findElement(By.xpath(addToCartBtnXpath));
+        addToCartBtn.click();
+        waitForPageToLoad();
+    }
 }
